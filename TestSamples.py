d91fa3e562c075116cc3e1e8304e01fdf0f550ed
Jared Merten <jarmerten@gmail.com>
2016-05-13 13:59:37 -0500

    Made new test

--- TestSamples.py
+++ TestSamples.py
@@ -10,27 +10,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_main(self):
         filelocation = "C:\\Users\\Jared Merten\\Documents\\package.zip"
-        TEst.replace_original()
-
-    def test_valid_path(self):
-        filelocation = "C:\\Users\\Jared Merten\\Documents\\package.zip"
-        TEst.check_valid_path(filelocation)
-        self.assertTrue(os.path.exists(filelocation), True)
-        TEst.replace_original()
-
-
-    def test_valid_path2(self):
-        filelocation = "C:\\Users\\Jared Merten\\Documents\\package.zip"
         TEst.check_valid_path(filelocation)
         self.assertEqual(filelocation, "C:\\Users\\Jared Merten\\Documents\\package.zip")
-        TEst.replace_original()
 
-
-    def test_manfest(self):
-        filelocation = "C:\\Users\\Jared Merten\\Documents\\package.zip"
-        TEst.open_manifest(filelocation)
-        self.assertEqual(filelocation, "C:\\Users\\Jared Merten\\Documents\\package.zip")
-        TEst.replace_original()
 
 if __name__ == '__main__':
     unittest.main()
