1c41d129d4d0a9e330a9558ba1ea1d3645e23bfd
Jared Merten <jarmerten@gmail.com>
2016-05-13 14:03:54 -0500

    Added new tests

--- TestSamples.py
+++ TestSamples.py
@@ -8,11 +8,15 @@ from picturestofiles import TEst
 class MyTestCase(unittest.TestCase):
 
 
-    def test_main(self):
+    def test_check_valid_path(self):
         filelocation = "C:\\Users\\Jared Merten\\Documents\\package.zip"
         TEst.check_valid_path(filelocation)
         self.assertEqual(filelocation, "C:\\Users\\Jared Merten\\Documents\\package.zip")
 
+    def test_check_valid_path1(self):
+        filelocation = "C:\\Users\\Jared Merten\\Documents\\packag"
+        TEst.check_valid_path(filelocation)
+        self.assertEqual(os.path.exists(filelocation), False)
 
 if __name__ == '__main__':
     unittest.main()
